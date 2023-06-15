b0f1c8f7b138a4c60db07628cc48c69603d8bcb4
RamoHasuni <omarinusah98@gmail.com>
2023-06-15 10:20:31 -0700

    6

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <unistd.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <string.h>
-#include <sys/stat.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: 1 (Failure)
+ */
 int main(void)
 {
-    const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    int fd = open("error.txt", O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-    write(fd, message, strlen(message));
-    close(fd);
-    return 1;
+	const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, message, 59);
+	return (1);
 }
